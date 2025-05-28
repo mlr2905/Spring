bb5ddfd880c87f5977ed4e372cefeee5bd53582e
mlr2905 <7585474@gmail.com>
2025-05-29 01:31:23 +0300

    up
    This reverts commit 7de4dad73d15c9744934649cc90d903883beafa7.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,15 +1,26 @@
-CREATE TABLE IF NOT EXISTS user (
-    id integer NOT NULL DEFAULT nextval('user_id_seq'::regclass),
+
+-- DROP TABLE IF EXISTS public.users;
+
+CREATE TABLE IF NOT EXISTS public.users
+(
+    id integer NOT NULL DEFAULT nextval('users_id_seq'::regclass),
     username character varying(60) COLLATE pg_catalog."default" NOT NULL,
-    password character varying COLLATE pg_catalog."default",
     email character varying(320) COLLATE pg_catalog."default" NOT NULL,
     role_id integer NOT NULL,
-    CONSTRAINT user_pkey PRIMARY KEY (id),
-    CONSTRAINT user_emall_key UNIQUE (email)
+    mongo_id character varying COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT users_pkey PRIMARY KEY (id),
+    CONSTRAINT users_emall_key UNIQUE (email)
         INCLUDE(email),
-    CONSTRAINT user_username_key UNIQUE (username),
+    CONSTRAINT users_username_key UNIQUE (username),
     CONSTRAINT role_id FOREIGN KEY (role_id)
         REFERENCES public.roles (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
-);
+        NOT VALID
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.users
+    OWNER to cloud_pgsql_user;
+
